c94f604cba9590ac27d52c2011f5246dc6ac8844
Alejandro Payan <alejandro.payan@sigob.com.mx>
2024-04-13 17:13:27 -0700

    validacion correos

--- index.js
+++ index.js
@@ -22,10 +22,20 @@ const JWT_SECRET = process.env.JWT_SECRET || "token.01010101";
 app.use(cors());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://luxuryhotels.businesscodecreators.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Origin', 'https://luxuryhotels.businesscodecreators.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://luxuryhotels.businesscodecreators.com/signup');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 
 app.use(express.json());
 import bodyParser from "body-parser";
